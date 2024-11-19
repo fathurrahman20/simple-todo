73f6c6b5ec212c5d475249361996e06599d4386a
Fathurrahman <10190031@bsi.ac.id>
2024-11-19 23:32:21 +0700

    fix: adjust empty message style for todo in dark and light modes

--- src/components/EmptyTodo.tsx
+++ src/components/EmptyTodo.tsx
@@ -5,12 +5,14 @@ const EmptyTodo = ({ page }: { page: PageKey }) => {
   return (
     <>
       <div> </div>
-      <main className="grid min-h-full px-6 py-24 mx-auto bg-white place-items-center sm:py-32 lg:px-8">
+      <main
+        className="grid min-h-full px-6 py-24 mx-auto bg-white divide-gray-200 
+  dark:bg-[#09090B] place-items-center sm:py-32 lg:px-8 -mt-6">
         <div className="mx-auto text-center">
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-gray-900 text-balance sm:text-7xl">
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-gray-900 dark:text-white text-balance sm:text-7xl">
             {message.title}
           </h1>
-          <p className="mt-6 text-lg font-medium text-gray-500 text-pretty sm:text-xl/8">
+          <p className="mt-6 text-lg font-medium text-gray-500 dark:text-white/70 text-pretty sm:text-xl/8">
             {message.description}
           </p>
         </div>
